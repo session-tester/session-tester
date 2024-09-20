97aa5c936abcb82c90ec104936425262379790f2
panzhongxian <panzhongxian0532@gmail.com>
2024-09-20 12:00:47 +0800

    Capture ck function automatically

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from .session import Session
 from .testcase import BatchTester, SingleRequestCase, SingleSessionCase, AllSessionCase
 from .tester import Tester
 from .user_info import UserInfo
+from .utils import stop_till_n_repeat, auto_gen_cases_from_ck_func
 
 __all__ = ["BatchSender", "Client", "Session", "UserInfo", "load_sessions", "BatchTester", "SingleRequestCase",
-           "SingleSessionCase", "AllSessionCase", "Tester"]
+           "SingleSessionCase", "AllSessionCase", "Tester", "stop_till_n_repeat", "auto_gen_cases_from_ck_func"]
--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
-from client import Session
+import ast
+import inspect
+import sys
+from typing import List
+
+from client import Session, SingleSessionCase, SingleRequestCase, AllSessionCase
+from client.session import HttpTransaction
 
 
 def stop_till_n_repeat(n):
@@ -6,3 +12,34 @@ def stop_till_n_repeat(n):
         return len(s.transactions) >= n
 
     return session_stop_func
+
+
+def auto_gen_cases_from_ck_func():
+    module = sys.modules["__main__"]
+    source = inspect.getsource(module)
+    tree = ast.parse(source)
+
+    check_cases = []
+
+    for node in ast.walk(tree):
+        if isinstance(node, ast.FunctionDef) and node.name.startswith('ck'):
+            func = getattr(module, node.name)
+            signature = inspect.signature(func)
+            params = list(signature.parameters.values())
+            if params:
+                if params[0].annotation == HttpTransaction:
+                    # 单请求
+                    check_cases.append(SingleRequestCase(rsp_checker=func))
+                elif params[0].annotation == Session:
+                    # 单会话
+                    check_cases.append(SingleSessionCase(session_checker=func))
+                elif params[0].annotation == List[Session]:
+                    # 所有会话
+                    check_cases.append(AllSessionCase(session_list_checker=func))
+                else:
+                    raise ValueError(
+                        f"First parameter of function {node.name} should be of type HttpTransaction or Session")
+            else:
+                raise ValueError(f"Function {node.name} should have at least one parameter")
+
+    return check_cases
