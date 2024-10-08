5dcd18021ea660a0d705e9928eb3f6d769cb27c6
panzhongxian <panzhongxian0532@gmail.com>
2024-10-08 20:12:32 +0800

    export SessionMaintainerSimple

--- session_tester/__init__.py
+++ session_tester/__init__.py
@@ -1,6 +1,6 @@
 from .client import Client
 from .session import Session, HttpTransaction
-from .session_maintainer import SessionMaintainerBase
+from .session_maintainer import SessionMaintainerBase, SessionMaintainerSimple, sm_n_rounds, sm_no_update, sm_no_init
 from .test_suite import TestSuite
 from .testcase import SingleRequestCase, SingleSessionCase, AllSessionCase, CheckResult
 from .tester import Tester
@@ -8,4 +8,5 @@ from .user_info import UserInfo
 from .utils import auto_gen_cases_from_chk_func
 
 __all__ = ["Client", "Session", "UserInfo", "SingleRequestCase", "SingleSessionCase", "AllSessionCase", "Tester",
-           "auto_gen_cases_from_chk_func", "TestSuite", "CheckResult", "HttpTransaction", "SessionMaintainerBase"]
+           "auto_gen_cases_from_chk_func", "TestSuite", "CheckResult", "HttpTransaction", "SessionMaintainerBase",
+           "SessionMaintainerSimple", "sm_n_rounds", "sm_no_update", "sm_no_init"]
