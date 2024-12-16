262c4e147e75f9ab4c086861215d2dd1ee282317
panzhongxian <panzhongxian0532@gmail.com>
2024-12-16 17:02:22 +0800

    Update pipy version to RC2

--- session_tester/cases/dist_stat.py
+++ session_tester/cases/dist_stat.py
@@ -4,19 +4,32 @@ from session_tester import AllSessionCase, Session, CheckResult, utils
 
 
 class HttpTransactionDistStatAllSessionCase(AllSessionCase):
-    def __init__(self, name: str = None, expectation: str = None, tag_get_func=None):
+    """ Tag分布，不带校验 """
+
+    def __init__(self, name: str = None, expectation: str = None, tag_get_func=None, filter_func=None):
+        """
+        :param name: 测试名称
+        :param expectation: 预期结果
+        :param tag_get_func: 从Session中获取标签的函数
+        :param filter_func: 过滤函数，如果filter_func非空，则只有filter_func(s:Session)返回True的元素才会被统计
+        """
         super().__init__(name, expectation)
         self.tag_get_func = tag_get_func
+        self.filter_func = filter_func
 
     def check(self, ssl: List[Session]) -> CheckResult:
+        if self.filter_func is not None:
+            ssl = list(filter(self.filter_func, ssl))
         output = utils.transaction_elem_dist_stat(ssl, self.tag_get_func)
         return CheckResult(True, "", output)
 
 
 class HttpTransactionDistStatCheckAllSessionCase(AllSessionCase):
-    def __init__(self, name: str = None, expectation: str = None, tag_get_func=None,
+    """ Tag分布，带校验 """
+
+    def __init__(self, name: str = None, expectation: str = None, tag_get_func=None, filter_func=None,
                  dist_expectation: Dict = None):
-        super().__init__(name, expectation)
+        super().__init__(name, expectation, filter_func)
         self.tag_get_func = tag_get_func
         self.dist_expectation = dist_expectation
 
